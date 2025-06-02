69205315e744213afb2801e69cce779df6538d8e
Rawsab <rawsab04@gmail.com>
2025-06-01 23:44:41 -0400

    add HTTPS auth and fix CORS origin

--- api/main.py
+++ api/main.py
@@ -7,17 +7,15 @@ app = FastAPI()
 # Add CORS middleware for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://patchly.vercel.app"
-    "https://patchly-gctzx1kc7-rawsabs-projects.vercel.app"
-    ],
+    # allow_origins=[
+    #     "http://localhost:5173",
+    #     "http://localhost:3000",
+    # ],
+    allow_origin_regex=r"https:\/\/patchly-[a-zA-Z0-9-]+\.vercel\.app",
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"],
-    max_age=600,  # Cache preflight requests for 10 minutes
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"],
+    allow_headers=["*", "Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With", "sec-ch-ua", "sec-ch-ua-mobile", "sec-ch-ua-platform"],
+    expose_headers=["*"]
 )
 
 app.include_router(router)
