7241ed2bfc57998b40df375e8b53357869ad0c42
Rawsab <rawsab04@gmail.com>
2025-06-01 23:29:43 -0400

    update CORS

--- api/main.py
+++ api/main.py
@@ -8,15 +8,14 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "https://patchly.vercel.app",
-        "https://patchly-gqilgvauv-rawsabs-projects.vercel.app",
-        "https://*.vercel.app"
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://patchly.vercel.app"
+    "https://patchly-gctzx1kc7-rawsabs-projects.vercel.app"
     ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"],
-    allow_headers=["*", "Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With", "sec-ch-ua", "sec-ch-ua-mobile", "sec-ch-ua-platform"],
+    allow_methods=["*"],
+    allow_headers=["*"],
     expose_headers=["*"],
     max_age=600,  # Cache preflight requests for 10 minutes
 )
